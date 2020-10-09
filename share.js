b701272a09fcbb431157a8bf8a3d353c5a5d6aae
sk-0927 <48261869+sk-0927@users.noreply.github.com>
2020-10-09 22:46:36 +0530

    Update share.js

--- share.js
+++ share.js
@@ -6,7 +6,7 @@ let balance=investment ;
 let profit = c/100 ;
 let days=4;
 while (balance<1000000){
-        balance=balance + (Math.round(balance * profit));
+        balance=Math.round(balance*(1+profit));
         days += 1 ;
         }
 document.querySelector("h1").innerHTML="Great! You will be millionaire in just  " + (days-4) +" day";
